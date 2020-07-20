b5ca86a61cf6659a2ac1a663216741e0996f8769
biraj.borah <biraj.borah@FireEye.com>
2020-07-20 20:09:13 +0530

    adding the repository design pattern structure

---
+++ MyRepo/PaymentManagement/Repository/PaymentOrder.cs
@@ -0,0 +1,12 @@
+﻿using PaymentManagement.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentManagement.Repository
+{
+    public class PaymentOrder : IPaymentOrder
+    {
+    }
+}
