6d9bc8d2ea71817979b3eeffa9ef25a7ad6426e5
biraj.borah <biraj.borah@FireEye.com>
2020-07-20 21:00:04 +0530

    adding the dependency injection in unit test class

--- MyRepo/XUnitTestPaymentManagement/UnitTest1.cs
+++ MyRepo/XUnitTestPaymentManagement/UnitTest1.cs
@@ -1,3 +1,5 @@
+using PaymentManagement.Controllers;
+using PaymentManagement.Repository.IRepository;
 using System;
 using Xunit;
 
@@ -5,6 +7,14 @@ namespace XUnitTestPaymentManagement
 {
     public class UnitTest1
     {
+        PaymentController _controller;
+        IPaymentOrder _service;
+        public UnitTest1()
+        {
+            _service = new FakePaymentOrder();
+            _controller = new PaymentController(_service);
+        }
+
         [Fact]
         public void Test1()
         {
