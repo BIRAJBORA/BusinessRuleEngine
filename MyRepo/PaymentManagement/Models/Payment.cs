50f5d3d0e4a6274869b9be9c1cb4cc3894159c74
biraj.borah <biraj.borah@FireEye.com>
2020-07-20 20:40:55 +0530

    correcting the payment model to handle the payment type more effectively

--- MyRepo/PaymentManagement/Models/Payment.cs
+++ MyRepo/PaymentManagement/Models/Payment.cs
@@ -5,10 +5,19 @@ using System.Threading.Tasks;
 
 namespace PaymentManagement.Models
 {
+    public enum PaymentType
+    {
+        PhysicalProduct = 1,
+        Book = 2,
+        Membership = 3,
+        UpgradToMembership = 4,
+        Video = 5
+
+    }
     public class Payment
     {
         public string OwnerEmail { get; set; }
-        public string PaymentType { get; set; }
+        public PaymentType PaymentType { get; set; }
         public Double Amount { get; set; }
         public string Video { get; set; }
     }
