5b2b9b621890b0093ac20e10925d3665f536a3a3
biraj.borah <biraj.borah@FireEye.com>
2020-07-20 20:12:24 +0530

    adding the interface definitions

--- MyRepo/PaymentManagement/Repository/IRepository/IPaymentOrder.cs
+++ MyRepo/PaymentManagement/Repository/IRepository/IPaymentOrder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PaymentManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,10 @@ namespace PaymentManagement.Repository.IRepository
 {
     public interface IPaymentOrder
     {
+        List<PackingSlip> makePaymentOrder(Payment payment);
 
+        Membership createNewMembership(Payment payment);
+
+        Membership updateMembership(Payment payment);
     }
 }
